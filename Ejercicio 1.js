7234b644cf0ad576dbf1d398b34939fa89fcbf28
MRM <55654081+ElenHast@users.noreply.github.com>
2020-09-17 19:58:16 -0300

    Update Ejercicio 1.js

--- Ejercicio 1.js
+++ Ejercicio 1.js
@@ -1,1 +1,11 @@
 /* 1- Hacer una funcion que dado un string o cadena de caracteres, genere una nueva cadena con las mismas palabras pero no pueden haber espacios en blanco antes de la primera palabra y despues de la ultima. Y debe haber un solo espacio en blanco entre palabras. */
+
+function ejercicio1(texto) {
+  let newStr = texto.replaceAll('.','').trimStart().trimEnd();
+  return newStr;
+  }
+console.log(ejercicio1("       Hola como andas amigo    .    . "));
+
+
+//Usos de trim, trimStart trimEnd = Sacar espacios del strin, del principio, del final.
+//Usos de replace, como dice la palabra reemplazar algo por otra cosa, si es mas complicado podria intentar usar regEx
